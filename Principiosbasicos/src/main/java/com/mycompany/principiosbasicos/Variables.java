cdb70959f590c49828d80212662773e786d0a997
Jay Osorio Web <jayosorio709@gmail.com>
2023-02-07 00:15:40 -0500

    Principios Basicos JAVA

---
+++ Principiosbasicos/src/main/java/com/mycompany/principiosbasicos/Variables.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.principiosbasicos;
+
+/**
+ *
+ * @author jayos
+ */
+public class Variables {
+    
+    public static void main(String[] args) {
+         
+        for(int i = 0; i < 10; i++){
+            System.out.println(i);
+            
+           
+        }
+    }
+}
