1b3fe012520039decf1857ae553ca0fb0a27e76f
Jay Osorio Web <jayosorio709@gmail.com>
2023-02-08 22:50:03 -0500

    Insertando datos para aprender POO Java

---
+++ POO/src/main/java/com/mycompany/poo/Main.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poo;
+
+/**
+ *
+ * @author jayos
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Persona persona1 = new Persona();
+        persona1.nombre = "yair";
+        persona1.apellido = "osorio";
+        persona1.edad = 31;
+        persona1.estado = "union libre";
+        persona1.estudio = true; 
+        
+        Persona persona2 = new Persona();
+        persona2.nombre = "maria";
+        persona2.apellido = "zapata";
+        persona2.edad = 26;
+        persona2.estado = "union libre";
+        persona2.estudio = false;
+        
+        System.out.println("persona1 = " + persona1.nombre);
+        System.out.println("persona2 = " + persona2.apellido);
+        
+        persona1.apellido = "osorio meza";
+        System.out.println("persona1 = " + persona1.apellido);
+    }
+}
---
+++ POO/src/main/java/com/mycompany/poo/Persona.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poo;
+
+/**
+ *
+ * @author jayos
+ */
+public class Persona {
+        
+        String nombre;
+        String apellido;
+        int edad;
+        String estado;
+        boolean estudio;
+    }
